d8d0414afdf72766f07d3cad845ee7a4260cb2ae
oleksandr_khomenko <oleksandr88khomenko@gmail.com>
2014-10-15 14:08:50 +0300

    some changes

--- src/DIO_soft/second_lecture/SecondLectureHW.java
+++ src/DIO_soft/second_lecture/SecondLectureHW.java
@@ -1,5 +1,7 @@
 package DIO_soft.second_lecture;
 
+import DIO_soft.third_lecture.MyExceptions;
+
 import java.util.Arrays;
 
 /**
@@ -9,29 +11,51 @@ import java.util.Arrays;
 public class SecondLectureHW {
     public static void main(String[] args) {
 
+        SecondLectureHW secondLectureHW = new SecondLectureHW();
+
         int[] firstArr = {1, 5, 4, 23, 65, 32, 78};
         int[] secondArr = {3, 5, 24, 4, 1, 2, 34, 45, 32, 5};
 
 
-        int[] united = union(firstArr, secondArr);
+        int[] united = new int[0];
+        try {
+            united = secondLectureHW.union(firstArr, secondArr);
+        } catch (MyExceptions e) {
+            System.out.println(e.getMessage());
+        }
         printArray(united);
         System.out.println("just union ");
 
 
-        int[] innerUnion = innerUnion(firstArr, secondArr);
+        int[] innerUnion = new int[0];
+        try {
+            innerUnion = secondLectureHW.innerUnion(firstArr, secondArr);
+        } catch (MyExceptions e) {
+            System.out.println(e.getMessage());
+        }
         printArray(innerUnion);
         System.out.println("inner union ");
 
 
-        int[] outerUnion = outerUnion(firstArr, secondArr);
+        int[] outerUnion = new int[0];
+        try {
+            outerUnion = secondLectureHW.outerUnion(firstArr, secondArr);
+        } catch (MyExceptions e) {
+            System.out.println(e.getMessage());
+        }
         printArray(outerUnion);
         System.out.println("outer union ");
     }
 
     //outer union
-    public static int[] outerUnion(int[] firstArr, int[] secondArr) {
+    public int[] outerUnion(int[] firstArr, int[] secondArr) throws MyExceptions {
 
-        int[] innerUnion = innerUnion(firstArr, secondArr);
+        if (firstArr == null || firstArr.length == 0 || secondArr == null || secondArr.length == 0)
+            throw new MyExceptions(firstArr, secondArr);
+
+        int[] innerUnion = new int[0];
+        innerUnion = innerUnion(firstArr, secondArr);
+
         int[] outerUnion = new int[(firstArr.length + secondArr.length) - innerUnion.length * 2 - 1];
         int[] union = union(firstArr, secondArr);
         Arrays.sort(innerUnion);
@@ -46,7 +70,10 @@ public class SecondLectureHW {
     }
 
     //inner union
-    public static int[] innerUnion(int[] firstArr, int[] secondArr) {
+    public int[] innerUnion(int[] firstArr, int[] secondArr) throws MyExceptions {
+
+        if (firstArr == null || firstArr.length == 0 || secondArr == null || secondArr.length == 0)
+            throw new MyExceptions(firstArr, secondArr);
 
         Arrays.sort(firstArr);
         Arrays.sort(secondArr);
@@ -73,12 +100,18 @@ public class SecondLectureHW {
     }
 
     //merging two arrays considering duplicates
-    public static int[] union(int[] firstArr, int[] secondArr) {
+    public int[] union(int[] firstArr, int[] secondArr) throws MyExceptions {
+
+        if (firstArr == null || firstArr.length == 0 || secondArr == null || secondArr.length == 0)
+            throw new MyExceptions(firstArr, secondArr);
 
         int flength = firstArr.length;
         int slength = secondArr.length;
-        int[] innerUnion = innerUnion(firstArr, secondArr);
-        int[] union = new int[(flength + slength-1)-innerUnion.length];
+        int[] innerUnion = new int[0];
+
+        innerUnion = innerUnion(firstArr, secondArr);
+
+        int[] union = new int[(flength + slength - 1) - innerUnion.length];
 
         int traceIndex = 0;
 
