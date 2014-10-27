baaa22d089c5346c4a669b6324ade47515e0425e
oleksandr_khomenko <oleksandr88khomenko@gmail.com>
2014-10-27 20:45:27 +0200

    added implementation based on collections

--- src/DIO_soft/third_lecture/MyExceptions.java
+++ src/DIO_soft/third_lecture/MyExceptions.java
@@ -1,5 +1,7 @@
 package DIO_soft.third_lecture;
 
+import java.util.List;
+
 /**
  * Created by oleksandr_khomenko on 14.10.2014.
  */
@@ -8,14 +10,14 @@ public class MyExceptions extends Exception {
     private String cause;
     boolean plus;
 
-    public MyExceptions(int[] firstArr, int[] secondArr) {
+    public MyExceptions(List firstArr, List secondArr) {
 
-        if (firstArr == null || firstArr.length == 0) {
+        if (firstArr == null || firstArr.size() == 0) {
             cause = "First argument is null or empty";
             plus = true;
         }
 
-        if (secondArr == null || secondArr.length == 0) {
+        if (secondArr == null || secondArr.size() == 0) {
             cause = "Second argument is null or empty";
             if (plus)
                 cause = "and second argument is null or empty";
